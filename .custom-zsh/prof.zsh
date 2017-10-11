8fc755cb680c928ee080bfff3452f5087c134679
Gabriel Dunn <gabe.dunn@shaw.ca>
2017-10-11 12:45:28 -0600

    removed output of adding ssh keys

--- .custom-zsh/prof.zsh
+++ .custom-zsh/prof.zsh
@@ -1,13 +1,26 @@
 # Custom profile for redxtech
 
 # Add ssh keys
+
+# allow globs to expand for (#qN) comparing
 setopt extendedglob
+
+# if ~/.ssh has any files matching *.pem, add them all.
 if [[ -n $HOME/.ssh/*.pem(#qN) ]]; then
-	ssh-add $HOME/.ssh/*.pem
-fi
-if [[ -n $HOME/.ssh/*_rsa(#qN) ]]; then
-        ssh-add $HOME/.ssh/*_rsa
+	# add all *.pem files, and silence the output.
+	ssh-add $HOME/.ssh/*.pem > /dev/null 2>&1
+	# if command fails, echo to STDOUT, because prevous command was silenced.
+	if [ $? -ne 0 ]; then
+    	echo "ssh-adding .pem files failed. check manually."
+	fi
 fi
 
-# Clear on startup
-clear
+# if ~/.ssh has any files matching *_rsa, add them all.
+if [[ -n $HOME/.ssh/*_rsa(#qN) ]]; then
+	# add all *.pem files, and silence the output.
+    ssh-add $HOME/.ssh/*_rsa > /dev/null 2>&1
+    # if command fails, echo to STDOUT, because prevous command was silenced.
+	if [ $? -ne 0 ]; then
+    	echo "ssh-adding _rsa files failed. check manually."
+	fi
+fi
