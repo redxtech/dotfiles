35fcd615fd7fdcf879e11e6eedefd80c7de8b13f
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-01-06 18:52:40 -0700

    add file mode changes

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -6,6 +6,10 @@ if test -e $OMF_CONFIG/path.fish
   source $OMF_CONFIG/path.fish
 end
 
+if test -e $OMF_CONFIG/env.fish
+  source $OMF_CONFIG/env.fish
+end
+
 if test -e $OMF_CONFIG/prof.fish
   source $OMF_CONFIG/prof.fish
 end
