a618f0dd9ecf46cfb076ad637ad9f19f504df503
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-01-18 10:52:33 -0700

    add kitty setup and set /usr/bin/nvim to nvim

--- .config/omf/env.fish
+++ .config/omf/env.fish
@@ -1,6 +1,12 @@
 set -xg fish_user_paths
+set -xg EDITOR /usr/bin/nvim
 
 # init nvm
 if test -n "(nvm)"
   nvm use default
+end
+
+# init kitty
+if test -n "(kitty)"
+  kitty + complete setup fish | source
 end
