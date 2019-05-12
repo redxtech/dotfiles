b7937e71d9e8a345994d59e40ec701070b050743
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-05-12 00:46:30 -0600

    add some of bootstrap

---
+++ .yadm/bs/ubuntu/pre-install.sh
@@ -0,0 +1,17 @@
+# Do actions before running apt.
+
+# add dart repos
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+
+# add elixir repos
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
+rm erlang-solutions_1.0_all.deb
+
+# add go repos
+sudo add-apt-repository ppa:longsleep/golang-backports
+
+sudo apt update
