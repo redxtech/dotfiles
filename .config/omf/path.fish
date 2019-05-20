4665e53c6c5430b76c694574d23421f68d7e0dde
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-05-20 04:54:34 -0600

    add cargo bin to path

--- .config/omf/path.fish
+++ .config/omf/path.fish
@@ -7,5 +7,15 @@ if test -d $HOME/.opam/system/bin
 end
 
 if test -d $HOME/.cargo/bin
- set -gx PATH $PATH $HOME/.cargo/bin
+  set -gx PATH $PATH $HOME/.cargo/bin
 end
+
+if type -q asdf
+  set -l _RUST_VERSION (asdf current rust | awk '{print $1}')
+  if test -d $_RUST_PATH
+    set -gx PATH $PATH $HOME/.asdf/installs/rust/$_RUST_VERSION/bin
+  else
+    echo nopers
+  end
+end
+
