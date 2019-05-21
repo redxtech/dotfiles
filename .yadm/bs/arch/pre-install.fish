11a383ac90d9303bca18ba36c43b95f7434fe69d
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-05-21 10:45:22 -0600

    add quotes to curls

--- .yadm/bs/arch/pre-install.fish
+++ .yadm/bs/arch/pre-install.fish
@@ -11,14 +11,14 @@ cd $HOME/pacaur-install
 # install dependencies
 mkdir $HOME/pacaur-install/auracle
 cd $HOME/pacaur-install/auracle
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=auracle-git
+curl -o PKGBUILD 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=auracle-git'
 makepkg PKGBUILD
 sudo pacman -U auracle*.tar.xz --noconfirm
 cd $HOME/pacaur-install
 rm -r $HOME/pacaur-install/auracle
 
 # grab pacaur pkgbuild and install it
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+curl -o PKGBUILD 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur'
 makepkg PKGBUILD
 sudo pacman -U pacaur*.tar.xz --noconfirm
 cd $HOME
