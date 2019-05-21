6cbdccd345dacd70485f51090ffb5cb4f1d81339
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-05-21 12:12:57 -0600

    make arch pre install commands run in quiet mode and make commands more verbose

--- .yadm/bs/arch/pre-install.fish
+++ .yadm/bs/arch/pre-install.fish
@@ -7,20 +7,17 @@ cd $HOME
 # install pacaur
 mkdir $HOME/pacaur-install
 cd $HOME/pacaur-install
-
 # install dependencies
 mkdir $HOME/pacaur-install/auracle
 cd $HOME/pacaur-install/auracle
 curl -o PKGBUILD 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=auracle-git'
-makepkg PKGBUILD
-sudo pacman -U auracle*.tar.xz --noconfirm
+makepkg --install --syncdeps PKGBUILD --noconfirm
 cd $HOME/pacaur-install
 rm -r $HOME/pacaur-install/auracle
 
 # grab pacaur pkgbuild and install it
 curl -o PKGBUILD 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur'
-makepkg PKGBUILD
-sudo pacman -U pacaur*.tar.xz --noconfirm
+makepkg --install --syncdeps PKGBUILD --noconfirm
 cd $HOME
 rm -r $HOME/pacaur-install
 
