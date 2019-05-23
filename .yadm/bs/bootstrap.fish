1f6e90d80a104033146e1607d2a2957c048b0323
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-05-23 10:50:23 -0600

    fix package installing

--- .yadm/bs/bootstrap.fish
+++ .yadm/bs/bootstrap.fish
@@ -13,17 +13,17 @@ source $HOME/.yadm/bs/$DISTRO/pre-install.fish
 
 echo Installing packages from system package manager...
 
-set -l PACKAGES_BASE cowsay docker docker-compose fish fzf gitflow-avh htop libwebp neovim openssh p7zip pacaur pakku pkgfile rclone rsync screenfetch thefuck time tldr wget yadm-git yay
-set -l PACKAGES_DESKTOP adobe-source-sans-pro-fonts aircrack-ng alacritty alacritty-terminfo atom awesome bitwarden calibre chromium-widevine code darkaudacity-git discord dolphin dolphin-megasync-git drill-search-git ffmpeg-full firefox firefox-developer-edition flac flameshot gimp gnome-terminal google-chrome gparted guake hyper inkscape jdownloader2 jetbrains-toolbox lastfm libreoffice-fresh mediainfo megasync mktorrent mktorrent octave openvpn otf-fira-code otf-fira-mono otf-fira-sans p7zip-gui plex-media-player postman pymedusa qbittorrent qdirstat sddm sddm-kcm sharenix-git spotify sublime-text synergy teamviewer thunderbird ttf-bitstream-vera ttf-emojione-color ttf-humor-sans ttf-ms-fonts ttf-roboto ttf-roboto-mono ttf-roboto-mono-powerline-git ttf-twemoji-color ttf-ubuntu-font-family variety vectr vivaldi vivaldi-ffmpeg-codecs vivaldi-snapshot vivaldi-snapshot-ffmpeg-codecs vlc wine yubico-yubioauth-desktio yubikey-manager yubikey-manager-qt yubikey-personalization-gui
-set -l PACKAGES_MAIN handbrake jackett lutris plex-media-server radarr sonarr steam
+set -l PACKAGES_BASE 'cowsay docker docker-compose fish fzf gitflow-avh htop libwebp neofetch neovim openssh p7zip pacaur pakku pkgfile rclone rsync thefuck time tldr wget yadm-git yay'
+set -l PACKAGES_DESKTOP 'adobe-source-sans-pro-fonts aircrack-ng alacritty alacritty-terminfo atom awesome bitwarden calibre chromium-widevine code darkaudacity-git discord dolphin dolphin-megasync-git drill-search-git ffmpeg firefox firefox-developer-edition flac flameshot gimp gnome-terminal google-chrome gparted guake inkscape jdownloader2 jetbrains-toolbox libreoffice-fresh mediainfo megasync mktorrent mktorrent octave openvpn otf-fira-code otf-fira-mono otf-fira-sans p7zip-gui plex-media-player pymedusa qbittorrent qdirstat sddm sddm-kcm sharenix-git spotify synergy teamviewer thunderbird ttf-bitstream-vera ttf-emojione-color ttf-humor-sans ttf-ms-fonts ttf-roboto ttf-roboto-mono ttf-roboto-mono-powerline-git ttf-twemoji-color ttf-ubuntu-font-family variety vectr vivaldi vivaldi-ffmpeg-codecs vivaldi-snapshot vivaldi-snapshot-ffmpeg-codecs vlc wine yubikey-manager yubikey-manager-qt yubikey-personalization-gui'
+set -l PACKAGES_MAIN 'handbrake jackett lutris plex-media-server radarr sonarr steam'
 
 # Choose proper package list.
-set -l PACKAGES $PACKAGES_BASE
+set -l PACKAGES "$PACKAGES_BASE"
 if test -f $HOME/.bs-desktop
-  set PACKAGES $PACKAGES $PACKAGES_DESKTOP
+  set PACKAGES "$PACKAGES $PACKAGES_DESKTOP"
 end
 if test -f $HOME/.bs-main
-  set PACKAGES $PACKAGES $PACKAGES_DESKTOP $PACKAGES_MAIN
+  set PACKAGES "$PACKAGES $PACKAGES_DESKTOP $PACKAGES_MAIN"
 end
 
 # Install packages based on distro.
