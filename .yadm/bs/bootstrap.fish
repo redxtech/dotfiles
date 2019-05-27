a3725c0bca0f2cc288e659562ce69b59cd62f9e5
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-05-27 12:16:43 -0600

    add pacman-contrib to default packages

--- .yadm/bs/bootstrap.fish
+++ .yadm/bs/bootstrap.fish
@@ -13,7 +13,7 @@ source $HOME/.yadm/bs/$DISTRO/pre-install.fish
 
 echo Installing packages from system package manager...
 
-set -l PACKAGES_BASE 'cmake cowsay docker docker-compose fish fzf gitflow-avh htop libwebp neofetch neovim openssh p7zip pacaur pakku pkgfile rclone rsync thefuck time tldr wget yadm-git yay'
+set -l PACKAGES_BASE 'cmake cowsay docker docker-compose fish fzf gitflow-avh htop libwebp neofetch neovim openssh p7zip pacaur pacman-contrib pakku pkgfile rclone rsync thefuck time tldr wget yadm-git yay'
 set -l PACKAGES_DESKTOP 'adobe-source-sans-pro-fonts aircrack-ng alacritty alacritty-terminfo atom awesome bitwarden calibre chromium-widevine code darkaudacity-git discord dolphin dolphin-megasync-git drill-search-git ffmpeg firefox firefox-developer-edition flac flameshot gimp gnome-terminal google-chrome gparted guake inkscape jdownloader2 jetbrains-toolbox libreoffice-fresh mediainfo megasync mktorrent mktorrent octave openvpn otf-fira-code otf-fira-mono otf-fira-sans p7zip-gui plex-media-player pymedusa qbittorrent qdirstat sddm sddm-kcm sharenix-git spotify synergy teamviewer thunderbird ttf-bitstream-vera ttf-emojione-color ttf-humor-sans ttf-ms-fonts ttf-roboto ttf-roboto-mono ttf-roboto-mono-powerline-git ttf-twemoji-color ttf-ubuntu-font-family variety vectr vivaldi vivaldi-ffmpeg-codecs vivaldi-snapshot vivaldi-snapshot-ffmpeg-codecs vlc wine yubikey-manager yubikey-manager-qt yubikey-personalization-gui'
 set -l PACKAGES_MAIN 'handbrake jackett lutris plex-media-server radarr sonarr steam'
 
