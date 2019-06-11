a111dbec9e133f483671a3c6212b27c8ab4a78e8
Gabe Dunn <gabe.dunn@shaw.ca>
2019-06-10 18:20:18 -0600

    add python to bin

--- .config/omf/path.fish
+++ .config/omf/path.fish
@@ -12,10 +12,15 @@ end
 
 if type -q asdf
   set -l _RUST_VERSION (asdf current rust | awk '{print $1}')
+  set -l _RUST_PATH $HOME/.asdf/installs/rust/$_RUST_VERSION/bin
   if test -d $_RUST_PATH
-    set -gx PATH $PATH $HOME/.asdf/installs/rust/$_RUST_VERSION/bin
-  else
-    echo nopers
+    set -gx PATH $PATH $_RUST_PATH
+  end
+  
+  set -l _PYTHON_VERSION (asdf current python | awk '{print $1}')
+  set -l _PYTHON_PATH $HOME/.asdf/installs/python/$_PYTHON_VERSION/bin
+  if test -d $_PYTHON_PATH
+    set -gx PATH $PATH $_PYTHON_PATH
   end
 end
 
