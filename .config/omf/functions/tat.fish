c2c5d44ab3e95b15f65d0484278446eb119682a1
Gabe Dunn <gabe.dunn@shaw.ca>
2019-10-01 23:26:56 -0700

    add tmux attach function to fish

---
+++ .config/omf/functions/tat.fish
@@ -0,0 +1,18 @@
+function tat -d "attach or create tmux session named the same as current directory"
+	# save the currect directory to be made into a session name
+	set -lx session_name (basename "$PWD" | tr . -)
+
+	# if the TMUX variable isn't set, make a new session
+	if test -z "$TMUX"
+		tmux new-session -As "$session_name"
+	# otherwise test if a session already exists with the same name
+	else
+		# if a session with the same name exists, attach to it
+		if tmux list-sessions | sed -E 's/:.*$//' | grep -q "^$session_name\$"
+			tmux switch-client -t "$session_name"
+		# otherwise create a new sessions
+		else
+			tmux new-session -Ad -s "$session_name"
+		end
+	end
+end
