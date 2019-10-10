ffbcd15a9c069218e9268aa8cb49623f51c724e6
Gabe Dunn <gabe.dunn@shaw.ca>
2019-10-10 00:43:02 -0700

    sorta fix path things

--- .config/omf/path.fish
+++ .config/omf/path.fish
@@ -1,26 +1,34 @@
-if test -d $HOME/.config/yarn/global/node_modules/.bin
-  set -gx PATH $PATH $HOME/.config/yarn/global/node_modules/.bin
+set -l yarn_path "$HOME/.config/yarn/global/node_modules/.bin"
+set -l opam_path "$HOME/.opam/system/bin"
+set -l cargo_path "$HOME/.cargo/bin"
+set -l python_path "$HOME/.asdf/installs/python"
+set -l rust_path "$HOME/.asdf/installs/rust"
+
+# echo $PATH | sed -E 's/ /\n/g'
+
+if test -d $yarn_path; and ! contains $yarn_path $PATH
+  set -gx PATH $PATH $yarn_path
 end
 
-if test -d $HOME/.opam/system/bin
-  set -gx PATH $PATH $HOME/.opam/system/bin
+if test -d $opam_path; and ! contains $opam_path $PATH
+  set -gx PATH $PATH $opam_path
 end
 
-if test -d $HOME/.cargo/bin
-  set -gx PATH $PATH $HOME/.cargo/bin
+if test -d $cargo_path; and ! contains $cargo_path 
+  set -gx PATH $PATH $cargo_path
 end
 
 if type -q asdf
-  set -l _RUST_VERSION (asdf current rust | awk '{print $1}')
-  set -l _RUST_PATH $HOME/.asdf/installs/rust/$_RUST_VERSION/bin
-  if test -d $_RUST_PATH
-    set -gx PATH $PATH $_RUST_PATH
+  set -l rust_version (asdf current rust | awk '{print $1}')
+  set -l rust_bin_path "$rust_path/$rust_version/bin"
+  if test -d $rust_bin_path; and ! contains $rust_bin_path $PATH
+    set -gx PATH $PATH $rust_bin_path
   end
   
-  set -l _PYTHON_VERSION (asdf current python | awk '{print $1}')
-  set -l _PYTHON_PATH $HOME/.asdf/installs/python/$_PYTHON_VERSION/bin
-  if test -d $_PYTHON_PATH
-    set -gx PATH $PATH $_PYTHON_PATH
+  set -l python_version (asdf current python | awk '{print $1}')
+  set -l python_bin_path "$python_path/$python_version/bin"
+  if test -d $python_bin_path; and ! contains $python_bin_path $PATH
+    set -gx PATH $PATH $python_bin_path
   end
 end
 
