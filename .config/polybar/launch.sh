ea8d83c9c7d07594d3e138fce1f54bedaaf175e0
Gabe Dunn <gabe.dunn@shaw.ca>
2019-10-14 17:25:27 -0700

    switch polybar bar choosing to regex

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,13 +6,13 @@ killall -q polybar
 # wait unti; the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if [[ $HOSTNAME == *"desktop"* ]]; then
+if [[ $HOSTNAME =~ "desktop" ]]; then
     # run the main polybar alongside the secondary polybar
     # for the second monitor
     echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
     polybar main >>/tmp/polybar1.log 2>&1 &
     polybar other >>/tmp/polybar2.log 2>&1 &
-elif [[ $HOSTNAME == *"laptop"* ]]; then
+elif [[ $HOSTNAME =~ "laptop" ]]; then
     # run the laptop polybar
     echo "---" | tee -a /tmp/polybar1.log
     polybar laptop >>/tmp/polybar1.log 2>&1 &
