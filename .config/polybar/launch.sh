5bbbb77120c731f65fb41fd968ad656faaea1014
Gabe Dunn <gabe.dunn@shaw.ca>
2019-10-14 17:22:18 -0700

    dynamically launch different polybar bars based on hostname

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,19 @@ killall -q polybar
 # wait unti; the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main >>/tmp/polybar1.log 2>&1 &
-polybar other >>/tmp/polybar2.log 2>&1 &
+if [[ $HOSTNAME == *"desktop"* ]]; then
+    # run the main polybar alongside the secondary polybar
+    # for the second monitor
+    echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+    polybar main >>/tmp/polybar1.log 2>&1 &
+    polybar other >>/tmp/polybar2.log 2>&1 &
+elif [[ $HOSTNAME == *"laptop"* ]]; then
+    # run the laptop polybar
+    echo "---" | tee -a /tmp/polybar1.log
+    polybar laptop >>/tmp/polybar1.log 2>&1 &
+else
+    # run laptop bar, since no other single screen options
+    echo "---" | tee -a /tmp/polybar1.log
+    polybar laptop >>/tmp/polybar1.log 2>&1 &
+fi
 
-echo "Bars launched..."
