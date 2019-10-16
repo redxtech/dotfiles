eaa301194d529295cf784521d9cc88761774b8ba
Gabe Dunn <gabe.dunn@shaw.ca>
2019-10-16 16:40:57 -0700

    update pms & ws functions & completions

--- .config/omf/completions/ws.fish
+++ .config/omf/completions/ws.fish
@@ -1,6 +1,9 @@
 # completions for ws function
-complete -c ws
-complete -c ws -f -a "restart" -d "restart webserver"
-complete -c ws -f -a "start" -d "start webserver"
-complete -c ws -f -a "stop" -d "stop webserver"
-complete -c ws -f -a "status" -d "show webserver status"
+complete -c ws -d "show nginx webserver status"
+complete -c ws -f -a "start" -d "start nginx webserver"
+complete -c ws -f -a "restart" -d "restart nginx webserver"
+complete -c ws -f -a "stop" -d "stop nginx webserver"
+complete -c ws -f -a "status" -d "show nginx webserver status"
+complete -c ws -f -a "enable" -d "enable and start nginx webserver"
+complete -c ws -f -a "disable" -d "disable and stop nginx webserver"
+
--- .config/omf/functions/pms.fish
+++ .config/omf/functions/pms.fish
@@ -1,20 +1,37 @@
-# function to handle plex media server operations
-function pms -d "manage plex media server"
+# function to manage plex media server operations
+function pms -d "manage custom plex media server"
 
     # exit if the pms service file doesn't exist
     if ! test -f "/usr/lib/systemd/system/pms.service"
-        echo "pms not found"
+        echo "error: pms not found"
         return 1
     end
 
-    # set up arguments: restart|rs, start|up, stop|down|dn, status
-    if test "$argv" = "restart" -o "$argv" = "rs"
-        sudo systemctl restart pms
-    else if test "$argv" = "start" -o "$argv" = "up"
-        sudo systemctl start pms
-    else if test "$argv" = "stop" -o "$argv" = "down" -o "$argv" = "dn"
-        sudo systemctl stop pms
-    else
+    # make sure $args exits and has exactly one element
+    if test -z "$argv"
         sudo systemctl status pms
+    else if test (count $argv) -ne 1
+        echo "error: specify only one operation"
+        return 1
+    end
+
+    # run the specified systemctl command for pms
+    switch $argv
+        case start up
+            sudo systemctl start pms
+        case restart rs
+            sudo systemctl restart pms
+        case stop down dn
+            sudo systemctl stop pms
+        case status
+            sudo systemctl status pms
+        case enable setup
+            sudo systemctl --now enable pms
+        case disable remove
+            sudo systemctl --now disable pms
+        case '*'
+            echo "error: that operation doesn't exist"
+            return 1
     end
 end
+
