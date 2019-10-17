e37b73cd95029deea19dc5b667362649f065c717
Gabe Dunn <gabe.dunn@shaw.ca>
2019-10-17 15:33:57 -0700

    add tm_spotify scripts

---
+++ .local/bin/tmux/helpers/spotify.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# check if the operating system is linux
+is_linux () {
+    [ "$(uname)" == "Linux" ]
+}
+
+# check if the spotifycli command exists
+has_spotify_cli () {
+    type spotifycli >/dev/null 2>&1
+}
+
+# if on linux and spotifycli exists, show the message
+spotify_msg () {
+    if is_linux; then
+        if has_spotify_cli; then
+            spotifycli "$1"
+        else
+            echo "error: tm_spotify needs spotify-cli linux (pip)"
+        fi
+    else
+        echo "error: tm_spotify only works on linux"
+    fi
+}
+
+
---
+++ .local/bin/tmux/tm_spotify.tmux
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+source "$HOME/.local/bin/tmux/helpers/spotify.sh"
+
+main () {
+    spotify_msg --status
+}
+
+main
+
