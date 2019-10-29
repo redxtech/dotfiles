c8bb2538289a47c7554abe4464146575c5367a58
Gabe Dunn <gabe.dunn@shaw.ca>
2019-10-29 16:18:54 -0700

    stop echoing path adds

--- .config/zsh/path.sh
+++ .config/zsh/path.sh
@@ -19,12 +19,10 @@ contains () {
 }
 
 if test -d "$YARN_PATH" && ! contains "$YARN_PATH" "$PATH"; then
-    echo "adding yarn"
     export PATH="$PATH:$YARN_PATH"
 fi
 
 if test -d "$CARGO_PATH" && ! contains "$CARGO_PATH" "$PATH"; then
-    echo "adding cargo"
     export PATH="$PATH:$CARGO_PATH"
 fi
 
@@ -32,25 +30,21 @@ if command -v asdf >/dev/null; then
   RUST_VERSION="$(asdf current rust | awk '{print $1}')"
   RUST_BIN_PATH="$RUST_PATH/$RUST_VERSION/bin"
   if test -d "$RUST_BIN_PATH" && ! contains "$RUST_BIN_PATH" "$PATH"; then
-      echo "adding rust"
       export PATH="$PATH:$RUST_BIN_PATH"
   fi
   
   PYTHON_VERSION=$(asdf current python | awk '{print $1}')
   PYTHON_BIN_PATH="$PYTHON_PATH/$PYTHON_VERSION/bin"
   if test -d "$PYTHON_BIN_PATH" && ! contains "$PYTHON_BIN_PATH" "$PATH"; then
-      echo "adding python"
     export PATH="$PATH:$PYTHON_BIN_PATH"
   fi
 fi
 
 if test -d "$SCRIPTS_PATH" && ! contains "$SCRIPTS_PATH" "$PATH"; then
-    echo "adding scripts"
     export PATH="$PATH:$SCRIPTS_PATH"
 fi
 
 if test -d "$LOCAL_BIN_PATH" && ! contains "$LOCAL_BIN_PATH" "$PATH"; then
-    echo "in local bin"
     PATH="$PATH:$(du "$HOME/.local/bin/" | cut -f2 | tr '\n' ':' | sed 's/:*$//')"
     export PATH="$PATH:$LOCAL_BIN_PATH"
 fi
