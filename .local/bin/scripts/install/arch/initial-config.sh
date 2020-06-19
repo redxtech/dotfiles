6fef5cf738bb648cf5bf0eb65d45f82a4a99fae5
Gabe Dunn <gabe@gabedunn.dev>
2020-06-19 12:44:18 -0600

    add some more to the install scripts

---
+++ .local/bin/scripts/install/arch/initial-config.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# function to run the initial configuration
+main () {
+    echo "Choosing pacman mirrorlist mirrors."
+
+    # generate those
+
+    echo "Adding herecura repo"
+
+    # do that
+
+    # TODO: use sed to modify config files
+    # TODO: look into ccache
+    # TODO: change default core count for makepkg
+    # TODO: set sh to point to dash
+
+}
+
+main
+
--- .local/bin/scripts/install/arch/install.sh
+++ .local/bin/scripts/install/arch/install.sh
@@ -5,28 +5,44 @@ main () {
     echo "Started arch install."
 
     echo Step 01: install aur helper
-    # shellcheck source=./aur_helper.sh
-    . "$INSTALL_SCRIPT_DIR/arch/aur_helper.sh"
+    # shellcheck source=./aur-helper.sh
+    . "$INSTALL_SCRIPT_DIR/arch/aur-helper.sh"
 
     echo "Step 02: specify initial config"
-    # # shellcheck source=./initial-config.sh
-    # . "$INSTALL_SCRIPT_DIR/arch/initial-config.sh"
+    # shellcheck source=./initial-config.sh
+    . "$INSTALL_SCRIPT_DIR/arch/initial-config.sh"
 
-    echo "Step 03: install packages"
-    # # shellcheck source=./packages.sh
-    # . "$INSTALL_SCRIPT_DIR/arch/packages.sh"
-
-    echo "Step 04: install oh-my-zsh"
+    echo "Step 03: install oh-my-zsh"
     # # shellcheck source=./oh-my-zsh.sh
     # . "$INSTALL_SCRIPT_DIR/arch/oh-my-zsh.sh"
 
-    echo "Step 05: clone dotfiles"
+    echo "Step 04: clone dotfiles"
     # # shellcheck source=./dotfiles.sh
     # . "$INSTALL_SCRIPT_DIR/arch/dotfiles.sh"
     
-    echo "Step 06: next step"
-    # # shellcheck source=./.sh
-    # . "$INSTALL_SCRIPT_DIR/arch/.sh"
+    echo "Step 05: install wayland"
+    # # shellcheck source=./wayland.sh
+    # . "$INSTALL_SCRIPT_DIR/arch/wayland.sh"
+    
+    echo "Step 06: install desktop environment (GNOME)"
+    # # shellcheck source=./desktop-env.sh
+    # . "$INSTALL_SCRIPT_DIR/arch/desktop_env.sh"
+    
+    echo "Step 07: set DE config with gsettings"
+    # # shellcheck source=./gsettings.sh
+    # . "$INSTALL_SCRIPT_DIR/arch/gsettings.sh"
+    
+    echo "Step 08: install packages"
+    # # shellcheck source=./packages.sh
+    # . "$INSTALL_SCRIPT_DIR/arch/packages.sh"
+
+    echo "Step 09: initialize rclone mounts"
+    # # shellcheck source=./rclone-mounts.sh
+    # . "$INSTALL_SCRIPT_DIR/arch/rclone-mounts.sh"
+
+    echo "Step 10: initialize sonarr-sync"
+    # # shellcheck source=./rclone-mounts.sh
+    # . "$INSTALL_SCRIPT_DIR/arch/rclone-mounts.sh"
 }
 
 main
