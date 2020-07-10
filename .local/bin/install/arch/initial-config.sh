703b8855a9412620ed3597987f06ba65f3c4c084
Gabe Dunn <gabe@gabedunn.dev>
2020-07-10 15:19:37 -0600

    update todos

--- .local/bin/install/arch/initial-config.sh
+++ .local/bin/install/arch/initial-config.sh
@@ -13,6 +13,8 @@ main () {
     # TODO: use sed to modify config files
     # TODO: modify BUILDENV in /etc/makepkg.conf to include ccache
     # TODO: modify MAKEFLAGS in /etc/makepkg.conf to include -j9 (number of threads + 1)
+    # TODO: MAKEFLAGS="-j$(nproc)"
+    # TODO: PKGEXT='.pkg.tar'
     # TODO: set sh to point to dash
 
 }
