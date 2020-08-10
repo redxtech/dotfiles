78f0ef3a28c2160ff1e668f973e1542ced5ddf99
Gabe Dunn <gabe@gabedunn.dev>
2020-08-10 05:06:59 -0600

    add colours to some commands

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -3,8 +3,10 @@
 # new makedir command
 alias mkd="mkdir -pv"
 
-# make grep use colour
+# make various commands use colour
 alias grep="grep --color"
+alias diff='diff --color=auto'
+alias ip='ip -color=auto'
 
 # redo last command as sudo
 alias shit='sudo $(fc -ln -1)'
@@ -36,6 +38,17 @@ alias SS="sudo systemctl"
 # alias docker="sudo docker"
 # alias docker-compose="sudo docker-compose"
 # alias docker-nuke="docker rm (docker ps -q -a) && docker rmi (docker images -q -a)"
+
+# man function to use colours
+man() {
+    LESS_TERMCAP_md=$'\e[01;31m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[01;44;33m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[01;32m' \
+    command man "$@"
+}
 
 # for fun
 alias starwars="telnet towel.blinkenlights.nl"
--- .config/zsh/env.sh
+++ .config/zsh/env.sh
@@ -21,3 +21,13 @@ if test -e "$HOME/.config/zsh/.env"; then
     . "$HOME/.config/zsh/.env"
 fi
 
+# make less show colours
+export LESS=-R
+export LESS_TERMCAP_mb=$'\E[1;31m'     # begin blink
+export LESS_TERMCAP_md=$'\E[1;36m'     # begin bold
+export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
+export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
+export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+
