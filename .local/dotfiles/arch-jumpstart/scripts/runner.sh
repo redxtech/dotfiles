3f8ca66f6af25cec71737b2edf2add75ea245f84
Gabe Dunn <gabed@hey.com>
2021-02-11 19:16:26 -0800

    fix jumpstart scripts

--- .local/dotfiles/arch-jumpstart/scripts/runner.sh
+++ .local/dotfiles/arch-jumpstart/scripts/runner.sh
@@ -6,19 +6,19 @@ main () {
     export JS_BASE
     
     # shellcheck source=./00-install-pacaur.sh
-    . "$JS_BASE/00-install-pacaur.sh"
+    . "$JS_BASE/scripts/00-install-pacaur.sh"
     
     # shellcheck source=./10-add-herecura-repo.sh
-    . "$JS_BASE/10-add-herecura-repo.sh"
+    . "$JS_BASE/scripts/10-add-herecura-repo.sh"
 
     # shellcheck source=./20-install-packages.sh
-    . "$JS_BASE/20-install-packages.sh"
+    . "$JS_BASE/scripts/20-install-packages.sh"
 
     # shellcheck source=./30-go-over-configuration.sh
-    . "$JS_BASE/30-go-over-configuration.sh"
+    . "$JS_BASE/scripts/30-go-over-configuration.sh"
 
     # shellcheck source=./40-start-services.sh
-    . "$JS_BASE/40-start-services.sh"
+    . "$JS_BASE/scripts/40-start-services.sh"
 }
 
 main
