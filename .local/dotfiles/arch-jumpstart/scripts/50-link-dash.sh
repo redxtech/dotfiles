39cb4e99e0e759d6d5de90311de20a20ca6bbded
Gabe Dunn <gabed@hey.com>
2021-02-14 15:03:37 -0800

    fix dash linker

--- .local/dotfiles/arch-jumpstart/scripts/50-link-dash.sh
+++ .local/dotfiles/arch-jumpstart/scripts/50-link-dash.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env sh
 
-link_dash () {
+main () {
     echo "Symlinking /usr/bin/sh to dash..."
     if command -v dash >/dev/null; then
         sudo ln -sfT dash /usr/bin/sh
     else
         echo "Dash not installed!"
     fi
-}
-
-main () {
-    link_dash
 }
 
 main
