edfffaed26fcf0df0b8a4f7dd6efcd5c2fb12c6f
Gabe Dunn <gabed@hey.com>
2021-02-14 13:48:17 -0800

    add alternative runner method

--- .local/dotfiles/arch-jumpstart/scripts/runner.sh
+++ .local/dotfiles/arch-jumpstart/scripts/runner.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env sh
 
+alternative () {
+    # set base path
+    JS_BASE="$(dirname "$(dirname -- "$(realpath "$0")")")"
+    export JS_BASE
+    
+    # loop through and run all scripts
+    # shellcheck disable=SC2231
+    for file in $JS_BASE/*.sh; do
+        if [ "$(basename "$file")" = "runner.sh" ]; then
+            # avoid running this file
+            true
+        else
+            sh "$file"
+        fi
+    done
+}
+
 main () {
     # set base path
     JS_BASE="$(dirname "$(dirname -- "$(realpath "$0")")")"
