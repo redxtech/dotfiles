ac64a52ed1248fbf3927f8ea1ea56aea937ba18b
Gabe Dunn <gabed@hey.com>
2021-02-16 06:17:38 -0800

    fix yadm remote link

--- .local/dotfiles/arch-jumpstart/scripts/85-switch-yadm-remote.sh
+++ .local/dotfiles/arch-jumpstart/scripts/85-switch-yadm-remote.sh
@@ -3,7 +3,7 @@
 main () {
     # remove https remote and add ssh one
     yadm remote remove origin
-    yadm remote add origin git@github.com:redxtech/dotfile.git
+    yadm remote add origin git@github.com:redxtech/dotfiles.git
 }
 
 main
