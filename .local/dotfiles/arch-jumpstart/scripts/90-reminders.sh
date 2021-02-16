3cdce556a5606bc86cc9320735df3e318d9752f1
Gabe Dunn <gabed@hey.com>
2021-02-15 18:31:04 -0800

    update todos

--- .local/dotfiles/arch-jumpstart/scripts/90-reminders.sh
+++ .local/dotfiles/arch-jumpstart/scripts/90-reminders.sh
@@ -17,17 +17,12 @@ main () {
     echo ""
     echo "Some general reminders:"
     echo "- Download id_rsa{,.pub} and save to ~/.ssh (700), with perms 600 and 644 (pub)."
-    echo "- Add rclone configs"
+    echo "- Add rclone configs (movies, tv, music, sonarr, radarr, gabe, backup, other)"
+    echo "- Add local computers to /etc/hosts (desktop, laptop, rpi)"
     echo "- Run build-coc"
 
-    echo ""
-    echo "Some TODO reminders:"
-    echo ""
-
-    echo "- Test zsh install"
-    echo "- Add list of rclone configs"
-    echo "- Add local computers to /etc/hosts"
-    echo "- Fix zsh plugin choosing"
+    # TODO:
+    # - Test zsh install
 
     language_versions
 }
