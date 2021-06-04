7bbb5ebf007feb655b854021ca32a3d868ecc56d
Gabe Dunn <gabed@hey.com>
2021-06-03 23:34:15 -0600

    add some stuff to the base module

---
+++ .local/dotfiles/components/resources/scripts/base-add-herecura.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# append the repo to pacman.conf
+main () {
+    
+    # check that the repo isn't already installed
+    if grep herecura /etc/pacman.conf; then
+        echo "Herecura already added!"
+    else
+        # shellcheck disable=SC2024
+        printf "# Custom servers\n[herecura]\nServer = https://repo.herecura.be/\$repo/\$arch" \
+            | sudo tee -a /etc/pacman.conf >/dev/null
+
+        # reload pacman database
+        sudo pacman -Sy
+    fi
+}
+
+main
+
+
