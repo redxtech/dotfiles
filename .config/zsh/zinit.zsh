1b6f2c759473749557c1d067f1dd19ef3a3a13b6
Gabe Dunn <gabed@hey.com>
2021-06-15 12:38:25 -0600

    switch to zinit

---
+++ .config/zsh/zinit.zsh
@@ -0,0 +1,131 @@
+#!/usr/bin/env zsh
+
+# install zinit if it isn't already installed
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+  print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+  command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+  command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+    print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+    print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
+
+# load zinit
+source "$HOME/.zinit/bin/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# load a few important annexes, without turbo
+zinit light-mode for \
+  zinit-zsh/z-a-rust \
+  zinit-zsh/z-a-as-monitor \
+  zinit-zsh/z-a-patch-dl \
+  zinit-zsh/z-a-bin-gem-node
+
+# zsh prompt
+# zinit load redxtech/zshred
+# zinit load ~/Code/zshred
+zinit ice depth=1
+zinit light romkatv/powerlevel10k
+
+# lazy-load plugins with turbo mode
+zinit wait lucid for \
+  ael-code/zsh-colored-man-pages \
+  Aloxaf/fzf-tab \
+  ajeetdsouza/zoxide \
+  axtl/gpg-agent.zsh \
+  laggardkernel/zsh-thefuck \
+  le0me55i/zsh-extract \
+  MenkeTechnologies/zsh-cargo-completion \
+  MichaelAquilina/zsh-auto-notify \
+  MichaelAquilina/zsh-emojis \
+  redxtech/zsh-aur-install \
+  redxtech/zsh-fzf-utils \
+  redxtech/zsh-not-vim \
+  redxtech/zsh-unix-simple \
+  Tarrasch/zsh-bd \
+  Tarrasch/zsh-colors \
+  voronkovich/gitignore.plugin.zsh \
+
+# currently disabled plugins
+# zpm-zsh/tmux
+
+# git library for themes and completions
+zinit snippet OMZL::completion.zsh
+zinit snippet OMZL::git.zsh
+
+# oh-my-zsh plugins (single file plugins only)
+zinit wait lucid for \
+  OMZP::command-not-found \
+  OMZP::copybuffer \
+  OMZP::docker-compose \
+  OMZP::encode64 \
+  OMZP::httpie \
+  OMZP::keychain \
+  OMZP::man \
+  OMZP::pip \
+  OMZP::ssh-agent \
+  OMZP::systemd \
+  OMZP::transfer \
+  OMZP::zsh_reload
+
+# oh-my-zsh plugins with full directory
+zinit ice svn
+zinit snippet OMZP::gitfast
+
+# can't load this one in turbo mode
+zinit light olets/zsh-abbr
+
+# git-fuzzy bin file
+zinit as"null" wait lucid for \
+  sbin"bin/git-fuzzy" \
+    bigH/git-fuzzy
+
+# alias tips
+zinit ice wait lucid from"gh-r" as"program"
+zinit light sei40kr/fast-alias-tips-bin
+zinit ice wait lucid
+zinit light sei40kr/zsh-fast-alias-tips
+
+# TODO: use zinit to auto pull in some binary files from github releases
+# zinit as"null" lucid from"gh-r" for \
+  # mv"exa* -> exa" sbin ogham/exa \
+  # mv"fd* -> fd" sbin"fd/fd" @sharkdp/fd \
+  # sbin"fzf" junegunn/fzf-bin
+# TODO: use zinit to load npm packages here
+
+# distro specific plugins
+CURRENT_DISTRO="$(lsb_release -is)"
+if test "$CURRENT_DISTRO" = "Arch" -o "$CURRENT_DISTRO" = "Garuda"; then
+  zinit ice wait lucid
+  zinit snippet OMZP::archlinux
+elif test "$CURRENT_DISTRO" = "openSUSE"; then
+  zinit ice wait lucid
+  zinit snippet OMZP::suse
+fi
+unset CURRENT_DISTRO
+
+# completions
+zinit ice as"completion"
+zinit snippet OMZP::docker/_docker
+zinit ice as"completion"
+zinit snippet OMZP::docker-compose/_docker-compose
+zinit ice as"completion"
+zinit snippet OMZP::fd/_fd
+zinit ice as"completion"
+zinit snippet OMZP::ripgrep/_ripgrep
+zinit ice atclone'./zplug.zsh'
+zinit light g-plane/zsh-yarn-autocompletions
+
+# autocompletion, syntax highlighting, and autosuggestions
+zinit wait lucid for \
+  atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+    zdharma/fast-syntax-highlighting \
+  blockf \
+    zsh-users/zsh-completions \
+  atload"!_zsh_autosuggest_start" \
+    zsh-users/zsh-autosuggestions
+
+# history substring search
+zinit ice wait"!1" lucid atload"bindkey '^[[A' history-substring-search-up; bindkey '^[[[B' history-substring-search-down"
+zinit load zsh-users/zsh-history-substring-search
+
--- .zshrc
+++ .zshrc
@@ -1,44 +1,65 @@
 #!/usr/bin/env zsh
 
-# zsh & antigen config
+# zsh & zinit config
+
+# enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  # source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+# fi
+
 
 # custom zsh config location
 ZSH_CUSTOM="$HOME/.config/zsh"
-ANTIGEN_PATH="$ZSH_CUSTOM/antigen"
 
 # zsh config
-HYPHEN_INSENSITIVE="true"
-ENABLE_CORRECTION="false"
 HISTFILE=~/.histfile
 HISTSIZE=100000
 SAVEHIST=100000
-setopt appendhistory append_history share_history histignorealldups autocd beep extendedglob notify histignorespace
+
+# zsh options
+setopt auto_cd                    # auto cd into dirs
+setopt append_history             # each shell adds its history on exit
+setopt hist_ignore_dups           # keep duplicates in the histfile, if not consecutive
+setopt hist_ignore_space          # prefix command with space to exclude from history
+setopt share_history              # share history betweeen shells
+setopt extended_glob              # include #, ^, & ~ in globbing
+setopt hist_reduce_blanks         # remove unnecessary spaces
+setopt no_correct                 # don't do corrections
+setopt notify                     # show backgrounded jobs immediately
+setopt prompt_subst               # expand functions in prompt
+setopt transient_rprompt          # don't include right prompt in history 
+setopt function_arg_zero          # set $0 for each function, script, etc
+setopt no_menu_complete              # autocomplete menu
+
+# vim keybindings
 bindkey -v
+
+# hyphen & case insensitive completions 
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z-_}={A-Za-z_-}'
 
 # plugin config
 zstyle :omz:plugins:keychain agents gpg,ssh
 zstyle :omz:plugins:keychain identities id_ed25519 # id_github
 zstyle :omz:plugins:keychain options --quiet
 zstyle :omz:plugins:ssh-agent agent-forwarding on
-zstyle ":morpho" screen-saver "pipes.sh"
-zstyle ":morpho" arguments "-p 4 -t 3 -r 10000"
-zstyle ":morpho" delay "600"
-zstyle ":morpho" check-interval "60"
-ZSH_DOTENV_PROMPT=false
+FAST_ALIAS_TIPS_PREFIX="$(tput setaf 4)Alias tip: "
 
-# load antigen & all antigen config
-source "$ANTIGEN_PATH/antigen.zsh"
-antigen init "$HOME/.antigenrc"
+# load zoxide (zinit wont use it for some reason)
+# (( $+commands[zoxide] )) && eval "$(zoxide init zsh)"
 
-# load zoxide (antigen wont use it for some reason)
-(( $+commands[zoxide] )) && eval "$(zoxide init zsh)"
+# all zinit config here
+source "$ZSH_CUSTOM/zinit.zsh" 
 
 # autoload & compile the functions if it hasn't been done in the last 24h
 autoload -Uz compinit
-if [[ -n "$HOME/.cache/zsh/zcompdump-$ZSH_VERSION"(#qN.mh+24) ]]; then
-  compinit -C -d "$HOME/.cache/zsh/zcompdump-$ZSH_VERSION"
+ZSH_COMPDUMP="$ZSH_CACHE_DIR/zcomp-$HOST"
+if [[ -n "$ZSH_COMPDUMP"(#qN.mh+24) ]]; then
+  compinit -C -d "$ZSH_COMPDUMP"
 fi
 
 # source the custom zsh environment
 # shellcheck source=.config/zsh/init.zsh
 [ -f "$ZSH_CUSTOM/init.zsh" ] && . "$ZSH_CUSTOM/init.zsh"
+
+# to customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
