2aac02c91b10a4bc3062d65317daf867c7ab1b88
Gabe Dunn <gabed@hey.com>
2021-08-24 12:18:31 -0600

    add screenlayout

---
+++ .screenlayout/desktop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output HDMI-0 --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-0 --primary --mode 2560x1440 --pos 0x351 --rotate normal --output DP-1 --off --output DVI-D-0 --off
