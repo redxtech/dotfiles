1843bcec3e68937818d30a4df58bd3b0d16599db
Gabe Dunn <gabed@hey.com>
2021-09-01 10:43:16 -0600

    move xrandr commands elsewhere

--- .config/awesome/scripts/autorun.sh
+++ .config/awesome/scripts/autorun.sh
@@ -19,11 +19,25 @@ function srun {
   $@&
 }
 
+# apply proper monitor layour and sizing
+if test "$(hostname)" = "desktop"; then
+  xrandr --output DVI-I-0 --off \
+    --output DVI-I-1 --off \
+    --output HDMI-0 --mode 3840x2160 --pos 2560x0 --rotate normal \
+    --output DP-0 --primary --mode 2560x1440 --pos 0x360 --rotate normal \
+    --output DP-1 --off \
+    --output DVI-D-0 --off
+elif test "$(hostname)" = "laptop"; then
+  xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output DP-1 --off \
+    --output DP-2 --off \
+    --output DP-3 --off
+fi
+
+# sh "~/.screenlayout/desktop.sh"
+
 # compositor
 run picom --experimental-backend
-
-# apply proper monitor layour and sizing
-run "~/.screenlayout/$(hostname).sh"
 
 # run natural scrolling script if on laptop
 if test "$(hostname)" = "laptop"; then
--- .screenlayout/desktop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output HDMI-0 --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-0 --primary --mode 2560x1440 --pos 0x360 --rotate normal --output DP-1 --off --output DVI-D-0 --off
