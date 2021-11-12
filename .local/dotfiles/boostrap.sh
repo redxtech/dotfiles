dcce931cbdc9159f9e45b48004d40ac0e8dd13f6
Gabe Dunn <gabed@hey.com>
2021-11-11 23:18:23 -0700

    update bootstrap script

--- .local/dotfiles/boostrap.sh
+++ .local/dotfiles/boostrap.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env sh
 
-echo hello
+# Run this script with:
+# sh <(curl -Ls dot.gabedunn.dev)
+
+echo "Starting bootstrap script..."
+
+sudo pacman -S \
+  git \
+  htop \
+  neovim \
+  vim \
+  zsh
+
+# pull in yadm for dotfiles
+curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm
+chmod a+x /usr/local/bin/yadm
+
+# clone in the dotfiles
+/usr/local/bin/yadm clone git@github.com:redxtech/dotfiles.git
+
+# remove the yadm binary
+rm /usr/local/bin/yadm
+
