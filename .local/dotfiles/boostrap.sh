c20fcddb5bc20c181f83985bda109d10493636e0
Gabe Dunn <gabed@hey.com>
2021-11-11 23:33:09 -0700

    update bootstrap script

--- .local/dotfiles/boostrap.sh
+++ .local/dotfiles/boostrap.sh
@@ -8,13 +8,14 @@ echo "Starting bootstrap script..."
 sudo pacman -S \
   git \
   htop \
+  keychain \
   neovim \
   vim \
   zsh
 
 # generate new ssh key
 ssh-keygen -t ed25519 -C "gabe@sent.at"
-ssh-add ~/.ssh/id_ed25519
+eval $(keychain --eval id_ed25519)
 
 # pull in yadm for dotfiles
 sudo curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm
