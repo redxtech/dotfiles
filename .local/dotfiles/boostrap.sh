a52be634b2040c1e82a2de99658457ab0e4e566e
Gabe Dunn <gabed@hey.com>
2021-11-25 20:59:57 -0700

    use yadm from repos instead of pulling in a binary

--- .local/dotfiles/boostrap.sh
+++ .local/dotfiles/boostrap.sh
@@ -5,6 +5,8 @@
 
 echo "Starting bootstrap script..."
 
+# install the required packages
+echo "Installing required packages..."
 sudo pacman -S \
   git \
   htop \
@@ -15,10 +17,12 @@ sudo pacman -S \
   python \
   vim \
   xclip \
+  yadm \
   yarn \
   zsh
 
 # generate new ssh key
+echo "Generating new ssh key..."
 ssh-keygen -t ed25519 -C "gabe@sent.at"
 eval $(keychain --eval id_ed25519)
 
@@ -30,19 +34,16 @@ echo "SSH key copied to clipboard!"
 # shellcheck disable=SC2162
 read "Add your ssh key to github, then press enter to continue" </dev/tty
 
-# pull in yadm for dotfiles
-sudo curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm
-sudo chmod a+x /usr/local/bin/yadm
-
 # clone in the dotfiles
-/usr/local/bin/yadm clone git@github.com:redxtech/dotfiles.git
+echo "Cloning dotfiles repo..."
+yadm clone git@github.com:redxtech/dotfiles.git
 
 # set user shell to zsh
+echo "Setting default shell to zsh..."
 chsh -s "$(which zsh)"
 
 echo
 
-# remove the yadm binary
-echo Remember to remove the yadm binary with:
-echo sudo rm /usr/local/bin/yadm
+echo "Bootstrap script has finished running!"
+echo "Let zsh initialize, then use load-module to complete the setup!"
 
