56271bc9327d6402c800cccd3a24d28d1eb4ec18
Gabe Dunn <gabed@hey.com>
2021-12-01 11:22:23 -0700

    set powermenu to use betterlockscreen

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -26,7 +26,8 @@ $shutdown" | rofi -dmenu\
 # Do something based on selected option
 if [ "$selected_option" == "$lock" ]
 then
-    /home/$USER/.config/bspwm/scripts/i3lock-fancy/i3lock-fancy.sh
+    # /home/$USER/.config/bspwm/scripts/i3lock-fancy/i3lock-fancy.sh
+    betterlockscreen -l dimblur
 elif [ "$selected_option" == "$logout" ]
 then
     bspc quit
