f33c5831af7f2c207d47d842165da20f489018d9
Gabe Dunn <gabed@hey.com>
2021-12-17 15:34:42 -0700

    use plugin for align stuff

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -1,58 +1,59 @@
 -- options
 
 -- globals
-vim.o.background            = "dark"
-vim.o.completeopt           = "menu,menuone,noselect"
-vim.o.hidden                = true
-vim.o.hlsearch              = true
-vim.o.mouse                 = "a"
-vim.o.ruler                 = true
-vim.o.scrolloff             = 5
-vim.o.showmode              = true
-vim.o.sidescrolloff         = 5
-vim.o.smartcase             = true
-vim.o.inccommand            = "split"
-vim.o.splitbelow            = true
-vim.o.splitright            = true
-vim.o.termguicolors         = true
-vim.o.undofile              = true
-vim.o.foldlevelstart        = 99
+vim.o.background     = "dark"
+vim.o.completeopt    = "menu,menuone,noselect"
+vim.o.hidden         = true
+vim.o.hlsearch       = true
+vim.o.mouse          = "a"
+vim.o.ruler          = true
+vim.o.scrolloff      = 5
+vim.o.showmode       = true
+vim.o.sidescrolloff  = 5
+vim.o.smartcase      = true
+vim.o.inccommand     = "split"
+vim.o.splitbelow     = true
+vim.o.splitright     = true
+vim.o.termguicolors  = true
+vim.o.undofile       = true
+vim.o.foldlevelstart = 99
 
 
 -- window
-vim.wo.number               = true
-vim.wo.relativenumber       = true
-vim.wo.signcolumn           = "yes"
-vim.wo.wrap                 = false
-vim.wo.cursorcolumn         = true
-vim.wo.foldmethod           = "expr"
-vim.wo.foldexpr             = "nvim_treesitter#foldexpr()"
+vim.wo.number         = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn     = "yes"
+vim.wo.wrap           = false
+vim.wo.cursorcolumn   = true
+vim.wo.foldmethod     = "expr"
+vim.wo.foldexpr       = "nvim_treesitter#foldexpr()"
 
 -- buffer
-local indent                = 2
-vim.o.tabstop               = indent
-vim.bo.tabstop              = indent
-vim.o.softtabstop           = indent
-vim.bo.softtabstop          = indent
-vim.o.shiftwidth            = indent
-vim.bo.shiftwidth           = indent
-vim.o.autoindent            = true
-vim.bo.autoindent           = true
-vim.o.expandtab             = true
-vim.bo.expandtab            = true
+local indent       = 2
+vim.o.tabstop      = indent
+vim.bo.tabstop     = indent
+vim.o.softtabstop  = indent
+vim.bo.softtabstop = indent
+vim.o.shiftwidth   = indent
+vim.bo.shiftwidth  = indent
+vim.o.autoindent   = true
+vim.bo.autoindent  = true
+vim.o.expandtab    = true
+vim.bo.expandtab   = true
 
 -- binds?
-vim.g.mapleader             = " "
-vim.b.mapleader             = " "
+vim.g.mapleader               = " "
+vim.b.mapleader               = " "
 vim.g.AutoPairsShortcutToggle = ""
 
 -- some config
 vim.g.dashboard_default_executive = 'telescope'
+vim.g.ale_fixers                  = {'prettier', 'eslint'}
 
 -- colours
 vim.cmd('colo dracula')
 -- vim.cmd('colo wal')
-vim.g.airline_theme         = "dracula"
+vim.g.airline_theme = "dracula"
 
 -- syntax highlighting
 vim.cmd('syntax on')
