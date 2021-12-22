ce7849d99d30f149b0ea78c8088b1128d91b02eb
Gabe Dunn <gabed@hey.com>
2021-12-22 03:18:32 -0700

    fix screenlayout script

---
+++ .screenlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-1 --mode 3840x2160 --pos 2560x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off
