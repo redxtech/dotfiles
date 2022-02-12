33318117fa7faa92087bc30b7b0fb366b9c8417c
Gabe Dunn <gabed@hey.com>
2022-02-11 21:10:35 -0700

    move plugins first in nvim

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,7 +1,7 @@
 -- pull in config modules
 
-require('opts')     -- set options for vim and vim plugins
 require('plugins')  -- plugin management
+require('opts')     -- set options for vim and vim plugins
 require('setup')    -- additional plugin config
 require('lsp')      -- language server setup
 require('binds')    -- keybinds
