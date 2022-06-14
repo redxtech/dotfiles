f28f444bf9d3311a9ee16816e563df9e576f879a
Gabe Dunn <gabe@sent.at>
2022-06-14 00:54:58 -0600

    add dracula theme to fzf & ranger

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -19,6 +19,8 @@ export BROWSER
 # program specific
 export RANGER_LOAD_DEFAULT_RC="FALSE"
 export BAT_THEME=Dracula
+export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+
 
 # TODO: see if plugin for this
 # source local env file
