034751c8c87562ad58118099481a4f11548fe78b
Gabe Dunn <gabe@sent.at>
2023-03-14 02:23:36 -0700

    add lightbulb and glow to nvim

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -10,9 +10,42 @@ return {
 			},
 		},
 	},
+	{
+		'kosayoda/nvim-lightbulb',
+		event = 'BufReadPost',
+		opts = {
+			autocmd = {
+				enabled = true,
+			},
+			float = {
+				enabled = true,
+			},
+		},
+	},
 	{ 'andrewradev/tagalong.vim', event = 'VeryLazy' },
+	{ 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
 	-- { 'itspriddle/vim-shellcheck', event = 'VeryLazy', },
 	-- { 'jparise/vim-graphql', event = 'VeryLazy', },
 	-- { 'kovetskiy/sxhkd-vim', event = 'VeryLazy', },
 	-- { 'tmux-plugins/vim-tmux', event = 'VeryLazy', },
+	-- {
+	--   'monaqa/dial.nvim',
+	--   keys = {
+	--     { '<C-a>', '<cmd>lua require("dial.map").inc_normal()<cr>', { desc = 'Increment', noremap = true } },
+	--     { '<C-x>', '<cmd>lua require("dial.map").dec_normal()<cr>', { desc = 'Decrement', noremap = true } },
+	--     { '<C-a>', '<cmd>lua require("dial.map").inc_visual()<cr>', { desc = 'Increment', noremap = true, mode = 'v' } },
+	--     { '<C-x>', '<cmd>lua require("dial.map").dec_visual()<cr>', { desc = 'Decrement', noremap = true }, mode = 'v' },
+	--     {
+	--       'g<C-a>',
+	--       '<cmd>lua require("dial.map").inc_gvisual()<cr>',
+	--       { desc = 'Increment', noremap = true, mode = 'v' },
+	--     },
+	--     {
+	--       'g<C-x>',
+	--       '<cmd>lua require("dial.map").dec_gvisual()<cr>',
+	--       { desc = 'Decrement', noremap = true },
+	--       mode = 'v',
+	--     },
+	--   },
+	-- },
 }
