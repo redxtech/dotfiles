b2f861e7c0114d3e4bdb0ef0445d3e366039969d
Gabe Dunn <gabe@sent.at>
2023-03-14 02:28:47 -0700

    use nvim nightly in zinit

--- .config/zsh/zinit.zsh
+++ .config/zsh/zinit.zsh
@@ -88,7 +88,7 @@ zinit wait lucid from'gh-r' as"command" for \
     ogham/exa \
   mv'rip* ripgrep' sbin'**/rg(.exe|) -> rg' \
     BurntSushi/ripgrep \
-  sbin"**/bin/nvim -> nvim" ver"" \
+  sbin"**/bin/nvim -> nvim" ver"nightly" \
     neovim/neovim \
   mv"yq* yq" sbin'**/yq(.exe|) -> yq' \
     mikefarah/yq
