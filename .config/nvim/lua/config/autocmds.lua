77234009f67c731e6ee950709e9996cb2e9071fc
Gabe Dunn <gabe@sent.at>
2023-03-14 13:09:54 -0700

    add nvim autocmd to disable spelling on bufread

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+	return vim.api.nvim_create_augroup('lazyvim_' .. name, { clear = true })
+end
+
+-- disable spelling on buffer read
+vim.api.nvim_create_autocmd('BufReadPost', {
+	group = augroup('no_spell'),
+	callback = function()
+		vim.opt_local.spell = false
+	end,
+})
