6874807f9b290ca66519ec4ff09b72b8ae7596aa
Gabe Dunn <gabe@sent.at>
2023-05-30 13:24:52 -0600

    nvim: add git blame and commands

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		'pwntester/octo.nvim',
+		-- event = 'VeryLazy',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-telescope/telescope.nvim',
+			'nvim-tree/nvim-web-devicons',
+		},
+		opts = {},
+	},
+	{
+		'f-person/git-blame.nvim',
+		event = 'VeryLazy',
+		init = function()
+			vim.g.gitblame_enabled = 0
+			vim.g.gitblame_virtual_text_column = 80
+		end,
+		keys = {
+			{ '<leader>gb', '<cmd>GitBlameToggle<cr>', desc = 'Blame line' },
+			{ '<leader>go', '<cmd>GitBlameOpenCommitURL<cr>', desc = 'Open line commit' },
+		},
+	},
+}
