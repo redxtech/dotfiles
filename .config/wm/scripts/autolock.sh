31d07f998f96bcc0e3e69344519f18e0b9831849
Gabe Dunn <gabe@sent.at>
2023-06-01 17:06:56 +0000

    wm: swap xautolock for xidlehook

--- .config/wm/scripts/autolock.sh
+++ .config/wm/scripts/autolock.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# lock the screen after 10 mins
-main () {
-  if ! pgrep -f "xautolock"; then
-    xautolock \
-      -corners --++ \
-      -locker "betterlockscreen -l dimblur"
-  fi
+main() {
+	export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+	export SECONDARY_DISPLAY="$(xrandr | grep -v 'primary' | awk '/ connected/{print $1}')"
+	# lock the screen after 10 mins
+	if ! pgrep -f "xidlehook"; then
+		xidlehook \
+			--not-when-fullscreen \
+			--not-when-audio \
+			--timer 600 'betterlockscreen -l dimblur' ''
+		# --timer 900 'xset dpms force off' ''
+	fi
 }
 
 main
-
