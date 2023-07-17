a939f16baa276152f93113305c04519400c05d14
Gabe Dunn <gabe@sent.at>
2023-07-17 17:34:59 -0600

    nvim: add yanky lazyvim plugin

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -22,7 +22,7 @@ require('lazy').setup({
 		{ import = 'lazyvim.plugins.extras.linting.eslint' },
 		{ import = 'lazyvim.plugins.extras.formatting.prettier' },
 		{ import = 'lazyvim.plugins.extras.coding.copilot' },
-		-- { import = 'lazyvim.plugins.extras.coding.yanky' },
+		{ import = 'lazyvim.plugins.extras.coding.yanky' },
 		{ import = 'lazyvim.plugins.extras.dap.core' },
 		{ import = 'lazyvim.plugins.extras.test.core' },
 		{ import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
