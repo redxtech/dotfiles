5da907a4129f905ef9b820a2f4903cfc25838fc9
Gabe Dunn <gabe@sent.at>
2023-07-17 14:36:07 -0600

    nvim: add toggleterm to edgy config

--- .config/nvim/lua/plugins/edgy.lua
+++ .config/nvim/lua/plugins/edgy.lua
@@ -24,6 +24,15 @@ return {
 				},
 				'neo-tree',
 			},
+			bottom = {
+				{
+					ft = 'toggleterm',
+					size = { height = 0.3 },
+					filter = function(buf, win)
+						return vim.api.nvim_win_get_config(win).relative == ''
+					end,
+				},
+			},
 		},
 	},
 }
