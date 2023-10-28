fea07e844c7806156640e01d5e8b5f62c0888e8e
Gabe Dunn <gabe@sent.at>
2023-10-28 14:16:40 -0700

    hm: add mpv conf and feh

--- .config/home-manager/modules/de/desktop-apps.nix
+++ .config/home-manager/modules/de/desktop-apps.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs; [
     # audacity
     discord
-    # firefox
+    firefox
     mercury-browser
     multiviewer-for-f1
     neovide
@@ -17,7 +17,63 @@
   ];
 
   programs = {
-    feh = { enable = false; };
-    mpv = { enable = false; };
+    feh.enable = true;
+
+    mpv = {
+      enable = true;
+
+      bindings = {
+        a = "vf toggle vflip";
+        g = "vf toggle hflip";
+
+        WHEEL_UP = "osd-msg-bar seek -10";
+        WHEEL_DOWN = "osd-msg-bar seek 10";
+        WHEEL_LEFT = "osd-msg-bar seek -5";
+        WHEEL_RIGHT = "osd-msg-bar seek 5";
+
+        ## script binds
+
+        # quality menu
+        F =
+          "script-binding quality_menu/video_formats_toggle #! Stream Quality > Video";
+        "Alt+f" =
+          "script-binding quality_menu/audio_formats_toggle #! Stream Quality > Audio";
+        "Ctrl+r" = "script-binding quality_menu/reload";
+
+        # webtorrent
+        p = "script-binding webtorrent/toggle-info";
+      };
+
+      config = {
+        fs = false;
+        autofit-larger = "90%x90%";
+        hwdec = "auto";
+        volume-max = 250;
+        keepaspect = true;
+      };
+
+      scripts = with pkgs.mpvScripts; [
+        autocrop
+        autoload
+        mpris
+        quality-menu
+        sponsorblock
+        thumbfast
+        uosc
+        webtorrent-mpv-hook
+        visualizer
+      ];
+
+      scriptOpts = {
+        autoload = {
+          disabled = false;
+          images = false;
+          videos = true;
+          audio = true;
+          ignore_hidden = true;
+        };
+        visualizer = { name = "showwaves"; };
+      };
+    };
   };
 }
