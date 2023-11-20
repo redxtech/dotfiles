f7becbdf6868dff71b8305693bfdbdfb7ee03eaf
Gabe Dunn <gabe@sent.at>
2023-11-20 13:33:06 -0700

    hm(polybar): add optional second bar for laptop with dual monitors

--- .config/home-manager/modules/laptop/polybar.nix
+++ .config/home-manager/modules/laptop/polybar.nix
@@ -1,129 +1,155 @@
 { config, pkgs, lib, ... }:
 
 let
-  insertAt = with pkgs.lib.lists;
-    index: value: list:
-    let
-      before = take index list;
-      after = drop index list;
-    in before ++ values ++ after;
-
   kittyRun =
     "${pkgs.bspwm}/bin/bspc rule -a kitty -o state=floating; ${pkgs.kitty}/bin/kitty --single-instance";
 in {
   services.polybar = {
     script = ''
       polybar main &
+
+      MONITOR_COUNT=$(${pkgs.xorg.xrandr}/bin/xrandr | ${pkgs.ripgrep}/bin/rg ' connected' | ${pkgs.coreutils}/bin/wc -l)
+
+      if test "$MONITOR_COUNT" = "2"; then
+        polybar secondary &
+      fi
     '';
 
-    settings = {
-      "module/backlight" = {
-        type = "internal/backlight";
+    settings = with builtins;
+      with lib.strings; {
+        "bar/secondary" = {
+          inherit (config.services.polybar.settings."bar/main")
+            width height line-size offset bottom fixed-center wm-restack
+            override-redirect enable-ipc background foreground cursor font;
 
-        card = "intel_backlight";
-        enable-scroll = true;
+          monitor = "DP-1";
 
-        format = {
-          underline = "\${colours.backlight";
-          prefix = {
-            text = "󰖨";
-            background = "\${colours.backlight}";
-            foreground = "\${colours.bg}";
-            padding = 1;
+          tray.position = "none";
+
+          modules = {
+            left =
+              concatStringsSep " " [ "bspwm" "margin" "polywins-secondary" ];
+            center = config.services.polybar.settings."bar/main".modules.center;
+            right = concatStringsSep " " [
+              config.services.polybar.settings."bar/main".modules.right
+              "margin"
+              "powermenu"
+            ];
           };
         };
-        label = {
-          text = "%percentage%%";
-          background = "\${colours.bg-alt}";
-          foreground = "\${colours.fg}";
-          padding = 1;
+        "module/polywins-secondary" = {
+          inherit (config.services.polybar.settings."module/polywins")
+            format label tail type;
+
+          exec = "${pkgs.polywins}/bin/polywins DP-1";
         };
-      };
-      "module/battery" = {
-        type = "internal/battery";
+        "module/backlight" = {
+          type = "internal/backlight";
 
-        battery = "BAT0";
-        adapter = "AC";
-        full-at = 98;
+          card = "intel_backlight";
+          enable-scroll = true;
 
-        ramp-capacity = [ "󰂃" "󰁺" "󰁻" "󰁼" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
-        ramp.capacity = {
-          foreground = "\${colours.fg}";
-          background = "\${colours.bg-alt}";
-          padding = 1;
-        };
-
-        animation-charging = [ "󰂃" "󰁺" "󰁻" "󰁼" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
-        animation.charging = {
-          foreground = "\${colours.fg}";
-          background = "\${colours.bg-alt}";
-          padding = 1;
-        };
-
-        format = {
-          charging = {
-            text = "<label-charging><animation-charging>";
-            underline = "\${colours.cyan}";
+          format = {
+            underline = "\${colours.backlight";
             prefix = {
-              text = "󰂄";
-              background = "\${colours.cyan}";
+              text = "󰖨";
+              background = "\${colours.backlight}";
               foreground = "\${colours.bg}";
               padding = 1;
             };
           };
-          discharging = {
-            text = "<label-discharging><ramp-capacity>";
-            underline = "\${colours.cyan}";
-            prefix = {
-              text = "󰁹";
-              background = "\${colours.cyan}";
-              foreground = "\${colours.bg}";
-              padding = 1;
-            };
-          };
-          full = {
-            text = "<label-full><ramp-capacity>";
-            underline = "\${colours.green}";
-            prefix = {
-              text = "󰁹";
-              background = "\${colours.green}";
-              foreground = "\${colours.bg}";
-              padding = 1;
-            };
-          };
-        };
-        label = {
-          charging = {
-            text = "<animation-charging>";
-            background = "\${colours.bg-alt}";
-            padding = 1;
-          };
-          discharging = {
-            text = "%percentage%%";
-            # text = "<ramp-capacity>";
-            background = "\${colours.bg-alt}";
-            padding = 1;
-          };
-          full = {
+          label = {
             text = "%percentage%%";
             background = "\${colours.bg-alt}";
+            foreground = "\${colours.fg}";
             padding = 1;
           };
         };
+        "module/battery" = {
+          type = "internal/battery";
+
+          battery = "BAT0";
+          adapter = "AC";
+          full-at = 98;
+
+          ramp-capacity = [ "󰂃" "󰁺" "󰁻" "󰁼" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          ramp.capacity = {
+            foreground = "\${colours.fg}";
+            background = "\${colours.bg-alt}";
+            padding = 1;
+          };
+
+          animation-charging = [ "󰂃" "󰁺" "󰁻" "󰁼" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          animation.charging = {
+            foreground = "\${colours.fg}";
+            background = "\${colours.bg-alt}";
+            padding = 1;
+          };
+
+          format = {
+            charging = {
+              text = "<label-charging><animation-charging>";
+              underline = "\${colours.cyan}";
+              prefix = {
+                text = "󰂄";
+                background = "\${colours.cyan}";
+                foreground = "\${colours.bg}";
+                padding = 1;
+              };
+            };
+            discharging = {
+              text = "<label-discharging><ramp-capacity>";
+              underline = "\${colours.cyan}";
+              prefix = {
+                text = "󰁹";
+                background = "\${colours.cyan}";
+                foreground = "\${colours.bg}";
+                padding = 1;
+              };
+            };
+            full = {
+              text = "<label-full><ramp-capacity>";
+              underline = "\${colours.green}";
+              prefix = {
+                text = "󰁹";
+                background = "\${colours.green}";
+                foreground = "\${colours.bg}";
+                padding = 1;
+              };
+            };
+          };
+          label = {
+            charging = {
+              text = "<animation-charging>";
+              background = "\${colours.bg-alt}";
+              padding = 1;
+            };
+            discharging = {
+              text = "%percentage%%";
+              # text = "<ramp-capacity>";
+              background = "\${colours.bg-alt}";
+              padding = 1;
+            };
+            full = {
+              text = "%percentage%%";
+              background = "\${colours.bg-alt}";
+              padding = 1;
+            };
+          };
+        };
+        # "module/updates-ipc" = {
+        #   hook = [
+        #     "${pkgs.updates-available-arch}/bin/updates-available-nix"
+        #   ];
+
+        #   click.left = "${kittyRun} ${pkgs.updates-install-arch}/bin/updates-install-nix; ${pkgs.polybar}/bin/polybar-msg action '#updates-ipc.hook.0' &";
+        # };
+        # "module/updates" = {
+        #   # TODO: find nix equivalent
+
+        #   exec = "echo 1";
+        #   click-left = "${kittyRun} ${pkgs.zsh}/bin/zsh -c \"nix-channel --update; echo Done - Press enter to exit; read\"";
+        # };
       };
-      # "module/updates-ipc" = {
-      #   hook = [
-      #     "${pkgs.updates-available-arch}/bin/updates-available-nix"
-      #   ];
-
-      #   click.left = "${kittyRun} ${pkgs.updates-install-arch}/bin/updates-install-nix; ${pkgs.polybar}/bin/polybar-msg action '#updates-ipc.hook.0' &";
-      # };
-      # "module/updates" = {
-      #   # TODO: find nix equivalent
-
-      #   exec = "echo 1";
-      #   click-left = "${kittyRun} ${pkgs.zsh}/bin/zsh -c \"nix-channel --update; echo Done - Press enter to exit; read\"";
-      # };
-    };
   };
 }
