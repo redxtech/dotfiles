0dbf3f8a810ac855af39ccf5bcb08a24e2fdc962
Gabe Dunn <gabe@sent.at>
2023-12-03 14:56:49 -0800

    hm: fix picom-jonaburg not having meta.mainProgram

--- .config/home-manager/modules/package-overlays.nix
+++ .config/home-manager/modules/package-overlays.nix
@@ -1,6 +1,8 @@
 final: prev:
 
 {
+  picom-jonaburg = prev.picom-jonaburg.overrideAttrs
+    (oldAttrs: { meta.mainProgram = "picom"; });
   vivaldi = prev.vivaldi.override {
     commandLineArgs = "--force-dark-mode";
     proprietaryCodecs = true;
